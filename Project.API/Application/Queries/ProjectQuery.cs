8ddf2ab3029dd5050eff4378a78241c4fd2d5d72
creasy-pita <hz_ljq@live.com>
2018-07-11 08:05:34 +0800

    ProjectQuery

---
+++ Project.API/Application/Queries/ProjectQuery.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Project.Domain.AggregatesModel;
+
+namespace Project.API.Application.Queries
+{
+    public class ProjectQuery : IProjectQuery
+    {
+        public Task<Domain.AggregatesModel.Project> GetProjectDetail(int projectId, int userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<Domain.AggregatesModel.Project>> GetProjectList(int userId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
